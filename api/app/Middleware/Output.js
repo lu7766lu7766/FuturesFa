ada5864d9409c3c928bebc439502f9bf32f25ccf
Jac <lu7766lu7766@gmail.com>
2019-05-06 14:41:43 +0800

    開發模式回傳query

--- api/app/Middleware/Output.js
+++ api/app/Middleware/Output.js
@@ -1,15 +1,22 @@
 'use strict'
 
+const Env = use('Env')
 const CommonCodes = use('ApiCodes/Common')
 
 class OutputMiddleware
 {
   async handle({response}, next) {
+    const querys = []
+    if (Env.get('NODE_ENV', 'development') === 'development') {
+      DB.on('query', _query => querys.push(_.pick(_query, ['bindings', 'sql'])))
+    }
     await next()
-    response.send({
+    const sendBody = {
       code: [CommonCodes.OK],
-      data: response._lazyBody.content
-    })
+      data: response._lazyBody.content,
+      querys: querys.length ? querys : ''
+    }
+    response.send(_.pickBy(sendBody))
   }
 }
 
