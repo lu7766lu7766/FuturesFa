e7cf30e6857248b4feb885fb42bb12c3130ba1f1
Jac <lu7766lu7766@gmail.com>
2019-05-15 00:28:30 +0800

    加權台指欄位新增

--- api/database/migrations/1557021002971_txo_schema.js
+++ api/database/migrations/1557021002971_txo_schema.js
@@ -8,8 +8,10 @@ class TxoSchema extends Schema
   up () {
     this.dropTableIfExists('txo')
     this.create('txo', (table) => {
+      table.integer('taiex', 10).default(0).comment('加權')
       table.integer('taiex_updown', 6).default(0).comment('加權漲跌')
       table.decimal('taiex_updown_range', 6, 2).default(0).comment('加權漲跌幅')
+      table.integer('mtx', 10).default(0).comment('台指')
       table.integer('mtx_updown', 6).default(0).comment('台指漲跌')
       table.decimal('mtx_updown_range', 6, 2).default(0).comment('台指漲跌幅')
       table.integer('major', 6).default(0).comment('大戶(夜)')
