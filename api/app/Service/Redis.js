fce7481fba809fe1ee68962f4cf8548dde1f3309
Jac <lu7766lu7766@gmail.com>
2019-06-03 15:07:51 +0800

    redis版更

--- api/app/Service/Redis.js
+++ api/app/Service/Redis.js
@@ -15,13 +15,12 @@ class RedisService
     let isLock = ((await Redis.get(lockKey)) === 'true')
     let time = await Redis.get(timeKey)
 
-    isDBSQLConsole &&
-    console.log(`
+    let message = `
     Key：${key}
     鎖定：${isLock}
     上次更新時間：${time}
     現在時間${moment().getDateTime()}
-    時間差：${moment().diff(moment(time), 'seconds')}`)
+    時間差：${moment().diff(moment(time), 'seconds')}`
 
     if (isLock)
     {
@@ -32,20 +31,23 @@ class RedisService
         {
           times++
           isLock = ((await Redis.get(lockKey)) === 'true')
+          // 若是一秒後執行沒有被鎖住的話，代表可能之前的人已拿到資料，可以直接從快取拿資料
           if (!isLock)
           {
-            isDBSQLConsole && console.log('Result:快取lock\n')
+            isDBSQLConsole && console.log(message + '\nResult:快取lock\n')
             res = JSON.parse(await Redis.get(key))
             session.put(redisKey, true)
             resolve(res)
           }
+          // 若是三次都被鎖住的話，代表可能永遠被鎖，直接ＤＢ拿取資料
           if (times++ > 1)
           {
-            isDBSQLConsole && console.log('Result:DB lock\n')
+            isDBSQLConsole && console.log(message + '\nResult:DB lock\n')
+            // 先解開防止有人在等，會重複從DB提取
+            await Redis.set(lockKey, 'false')
             res = await func()
             await Redis.set(timeKey, moment().getDateTime())
             await Redis.set(key, JSON.stringify(res))
-            await Redis.set(lockKey, 'false')
             session.put(redisKey, false)
             resolve(res)
             clearInterval(timer)
@@ -53,9 +55,10 @@ class RedisService
         }, 1000)
       })
     }
+    // 若是沒有上次更新時間，或上次更新時間與現在時間相差一定秒數以上，從ＤＢ拿取資料
     else if (!time || moment().diff(moment(time), 'seconds') >= secs)
     {
-      isDBSQLConsole && console.log('Result:DB\n')
+      isDBSQLConsole && console.log(message + '\nResult:DB\n')
       // await Redis.quit([key, timeKey])
       await Redis.set(lockKey, 'true')
       res = await func()
@@ -64,9 +67,10 @@ class RedisService
       await Redis.set(lockKey, 'false')
       session.put(redisKey, false)
     }
+    // 有上次更新時間，且時與現在時間相差在指定秒數以內，從快取取資料
     else
     {
-      isDBSQLConsole && console.log('Result:快取\n')
+      isDBSQLConsole && console.log(message + '\nResult:快取\n')
       res = JSON.parse(await Redis.get(key))
       session.put(redisKey, true)
     }
