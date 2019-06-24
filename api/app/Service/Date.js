c5f68f0460ff0bf64e257d2de314002e40c9c66f
Jac <lu7766lu7766@gmail.com>
2019-06-24 11:35:49 +0800

    date邏輯修改

--- api/app/Service/Date.js
+++ api/app/Service/Date.js
@@ -13,15 +13,12 @@ class DateService
     const weekSettleStartTime = moment(dateTime).day(3).format('YYYY-MM-DD 08:45:00')
     const weekSettleEndTime = moment(dateTime).day(3).format('YYYY-MM-DD 13:45:00')
     const isMonthSettleTime = moment(dateTime).isBetween(monthSettleStartTime, monthSettleEndTime, 'minute', '[]')
-    const isWeekSettleTime = moment(dateTime).isBetween(weekSettleStartTime, weekSettleEndTime, 'minute', '[]')
+    // const isWeekSettleTime = moment(dateTime).isBetween(weekSettleStartTime, weekSettleEndTime, 'minute', '[]')
     // 1號在周三之前，則不用多做扣除，若是大於等於週三，則週數進行扣一
     const subCount = moment(moment(dateTime).format('YYYY-MM-01')).day() < 3
       ? 0
       : 1
-
-    // const thisWeekMainMonth = moment(dateTime).format('MM')
-    // const nextWeekMainMonth = moment(dateTime).add(1, 'months').format('MM')
-
+    // 開盤日
     let date = moment(dateTime).isBefore(moment(dateTime).format('YYYY-MM-DD 14:00:00'))
       ? moment(dateTime).subtract(1, 'days')
       : moment(dateTime)
@@ -29,34 +26,38 @@ class DateService
     {
       date = date.subtract(1, 'days')
     }
+
+    const thisWednesday = moment(dateTime).day(3)
+    const nextWednesday = moment(dateTime).day(10)
+
     return {
       dateTime,
       date: date.getDate(),
       monthSettleStartTime,
       monthSettleEndTime,
+      // 是否為月結週
       isMonthSettleTime,
+      // 本週的結算開始時間
       weekSettleStartTime,
+      // 本週的結算結束時間
       weekSettleEndTime,
       // 周選頁的月份
-      mainMonth: moment().isBefore(weekSettleEndTime, 'minute') ||
-      (moment(dateTime).day(3).format('MM') === moment(dateTime).add(1, 'weeks').day(3).format('MM'))
-        ? moment(dateTime).format('MM')
-        : moment(dateTime).add(1, 'months').format('MM'),
-      // moment(dateTime).isBefore(monthSettleEndTime, 'minute')
-      // ? moment(dateTime).format('MM')
-      // : moment(dateTime).add(1, 'months').format('MM'),
+      mainMonth: moment().isBefore(weekSettleEndTime, 'minute')
+        ? thisWednesday.format('MM')
+        : nextWednesday.format('MM'),
       // 月選頁的月份
       subMonth: moment(dateTime).isBefore(monthSettleStartTime, 'minute')
         ? moment(dateTime).format('MM')
         : moment(dateTime).add(1, 'months').format('MM'),
+      // 週選的週數
       mainWeek: moment(dateTime).isBefore(weekSettleEndTime, 'minute')
-        ? (this.getWeekOfMonth(moment(dateTime).day(3)) - subCount) == 0
+        ? (this.getWeeksOfMonth(thisWednesday) - subCount) === 0
           ? Math.ceil(moment(dateTime).subtract(1, 'month').daysInMonth() / 7)
-          : this.getWeekOfMonth(moment(dateTime).day(3)) - subCount
-        : this.getWeekOfMonth(moment(dateTime).day(10)) - subCount,
-      subWeek: isWeekSettleTime
-        ? this.getWeekOfMonth(moment(dateTime).day(10)) - subCount
-        : null
+          : this.getWeeksOfMonth(thisWednesday) - subCount
+        : this.getWeeksOfMonth(nextWednesday) - subCount,
+      // subWeek: isWeekSettleTime
+      //   ? this.getWeeksOfMonth(moment(dateTime).day(10)) - subCount
+      //   : null
     }
   }
 
@@ -73,7 +74,7 @@ class DateService
   }
 
   // 取得日期為該月第幾週
-  getWeekOfMonth(date)
+  getWeeksOfMonth(date)
   {
     return Math.ceil(
       (moment(date).date() - (7 - moment(moment(date).format('YYYY-MM-01')).day())) / 7) + 1
