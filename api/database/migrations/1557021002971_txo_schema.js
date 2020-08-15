80da07b802bdc7403b6f72a18141dfdc86149789
Jac <lu7766lu7766@gmail.com>
2020-08-15 10:17:19 +0800

    少欄位

--- api/database/migrations/1557021002971_txo_schema.js
+++ api/database/migrations/1557021002971_txo_schema.js
@@ -20,6 +20,7 @@ class TxoSchema extends Schema
       table.decimal('total_c', 10, 2).default(0).comment('總C')
       table.decimal('total_p', 10, 2).default(0).comment('總P')
       table.decimal('differ_cp', 10, 2).default(0).comment('CP差額')
+      table.datetime('created_at').comment('傳送時間')
     })
   }
 
