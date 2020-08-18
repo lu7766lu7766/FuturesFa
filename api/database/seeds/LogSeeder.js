2ebe6e6aebee5f623287be119d86fb7694a23351
Jac <lu7766lu7766@gmail.com>
2020-08-19 00:13:33 +0800

    db seed

--- api/database/seeds/LogSeeder.js
+++ api/database/seeds/LogSeeder.js
@@ -14,37 +14,40 @@
 
 class LogSeeder {
   async run() {
+    const yesterday = moment()
+      .subtract(1, "days")
+      .getDate();
     await DB.table("futures_chip_log").insert({
-      date: "2020-07-15",
+      date: yesterday,
       major_chip_valume: "0",
       retail_chip_valume: "0",
       differ: "0",
-      created_at: "2020-07-15 15:00"
+      created_at: yesterday + " 15:00"
     });
     await DB.table("option_accumulation").insert({
-      date: "2020-07-15",
+      date: yesterday,
       name: "台指選07W4 C 10900",
       item: "10000",
       chip_valume: "0",
       price: "0.00",
-      created_at: "2020-07-15 15:00",
+      created_at: yesterday + " 15:00",
       week_mtx: "10000",
       mtx: "10000",
       is_big_volume: "0"
     });
     await DB.table("option_chip_log").insert({
-      date: "2020-01-01",
+      date: yesterday,
       total_c: "0.00",
       total_p: "0.00",
-      created_at: "2020-07-15 15:00"
+      created_at: yesterday + " 15:00"
     });
     await DB.table("option_log").insert({
-      date: "2020-07-15",
+      date: yesterday,
       name: "台指選07W4 C 10900",
       item: "10000",
       chip_valume: "0",
       price: "0.00",
-      created_at: "2020-07-15 15:00",
+      created_at: yesterday + " 15:00",
       week_mtx: "10000",
       mtx: "10000",
       is_big_volume: "0"
